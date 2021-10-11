044a05246b5d36737bbf18c98948c4eece2a5696
AmandaLiuNZ <amandaliunz@gmail.com>
2021-10-11 22:50:25 +1300

    Share Skill

--- MarsFramework/Test/Program.cs
+++ MarsFramework/Test/Program.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using MarsFramework.Pages;
+using NUnit.Framework;
 
 namespace MarsFramework
 {
@@ -9,15 +10,40 @@ namespace MarsFramework
         class User : Global.Base
         {
 
-            [Test]
-            public void Test()
+            [Test,Order(1)]
+            public void TestShareSkill()
             {
-
-
+                ShareSkill shareSkill = new ShareSkill();
+                ManageListings manageListings = new ManageListings();
+                test = extent.StartTest("ShareSkillTest");
+       
+                shareSkill.EnterShareSkill();
+                string assertType = "ShareSkill";
+                manageListings.AssertSkill(assertType);
             }
 
+            [Test,Order(2)]
+            public void TestEditShareSkill()
+            {
+                ShareSkill shareSkill = new ShareSkill();
+                ManageListings manageListings = new ManageListings();
+                test = extent.StartTest("EditShareSkillTest");
 
+                manageListings.Listings();
+                shareSkill.EditShareSkill();
+                string assertType = "EditShareSkill";
+                manageListings.AssertSkill(assertType);
+            }
 
+            [Test,Order(3)]
+            public void TestDeleteSkill()
+            {
+                ManageListings manageListings = new ManageListings();
+                test = extent.StartTest("DeleteSkillTest");
+
+                manageListings.DeleteSkill();
+                manageListings.AssertDeleteMsg();
+            }
         }
     }
 }
